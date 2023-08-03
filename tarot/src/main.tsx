566c58d60483f16aa9c0c901b712ed23989cff57
Ellie-Aghajani <z.aqajani@gmail.com>
2023-08-02 19:56:28 -0700

    import chakra

--- tarot/src/main.tsx
+++ tarot/src/main.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
 import * as ReactDOM from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
