43c3e5cd55ad2f50a57e39450354201be0b2864e
Ellie-Aghajani <z.aqajani@gmail.com>
2023-08-04 21:00:37 -0700

    add background color

--- tarot/src/App.tsx
+++ tarot/src/App.tsx
@@ -3,9 +3,9 @@ import { Button, ButtonGroup, Grid, GridItem } from "@chakra-ui/react";
 function App() {
   return(
     <Grid templateAreas={`'nav nav' 'aside main' `}>
-      <GridItem area='nav'> Nav</GridItem>
-      <GridItem area='aside'> aside</GridItem>
-      <GridItem area='main'> main</GridItem>
+      <GridItem area='nav' bg='red'> Nav</GridItem>
+      <GridItem area='aside' bg='blue'> aside</GridItem>
+      <GridItem area='main' bg='yellow'> main</GridItem>
 
     </Grid>
   ) ;
