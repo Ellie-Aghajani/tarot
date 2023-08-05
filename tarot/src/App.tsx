3ddad0e75cd74528edf6398f114c2ff8094db0e9
Ellie-Aghajani <z.aqajani@gmail.com>
2023-08-04 21:05:55 -0700

    responsive layout

--- tarot/src/App.tsx
+++ tarot/src/App.tsx
@@ -1,13 +1,26 @@
 import { Button, ButtonGroup, Grid, GridItem } from "@chakra-ui/react";
 
 function App() {
-  return(
-    <Grid templateAreas={`'nav nav' 'aside main' `}>
-      <GridItem area='nav' bg='red'> Nav</GridItem>
-      <GridItem area='aside' bg='blue'> aside</GridItem>
-      <GridItem area='main' bg='yellow'> main</GridItem>
-
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="red">
+        {" "}
+        Nav
+      </GridItem>
+      <GridItem area="aside" bg="blue">
+        {" "}
+        aside
+      </GridItem>
+      <GridItem area="main" bg="yellow">
+        {" "}
+        main
+      </GridItem>
     </Grid>
-  ) ;
-
+  );
+}
 export default App;
